e55b3ab39528b2a96509850420cc1161948d81f4
JorgeRS <jorge.rocamonde.s@gmail.com>
2021-04-23 12:28:52 +0200

    examenuf1291

---
+++ examenuf1291Serviroca/src/examenuf1291Serviroca/examenjorgersgithub.java
@@ -0,0 +1,11 @@
+package examenuf1291Serviroca;
+
+public class examenjorgersgithub {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hola esto es una prueba de que github funciona correctamente, un Saludo");
+	}
+	
+}
